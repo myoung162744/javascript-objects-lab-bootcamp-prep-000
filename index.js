e34fb4004a7fb977b416719788254046e0817099
Matthew Young <myoung162744@gmail.com>
2018-05-23 00:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
